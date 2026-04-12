9a7552657da217b95a8a36f4006a49e153087dd1
vinirlmarques <viniciusrlmarques@gmail.com>
2026-04-12 16:53:50 -0300

    Fix exercise 03

--- mod3_exercise1.lua
+++ mod3_exercise1.lua
@@ -2,10 +2,24 @@ local sampleNames = { "Maria", "Lucia", "Arthur", "Boris", "Newton" }
 local testN = 10000
 local users = {}
 
+function binarySearch(lowerName)
+  local inf = 1
+  local sup = #users
+  while inf <= sup do
+    local meio = math.floor((inf + sup) / 2)
+    if lowerName < users[meio].name then
+      sup = meio - 1
+    else
+      inf = meio + 1
+    end
+  end
+  return inf
+end
+
 function registerUser(name)
   local lowerName = string.lower(name)
-  table.insert(users, { id = #users, name = lowerName })
-  table.sort(users, function(a, b) return a.name < b.name end)
+  local pos = binarySearch(lowerName)
+  table.insert(users, pos, { id = #users, name = lowerName })
 end
 
 function registerUsers(names)
@@ -14,29 +28,13 @@ function registerUsers(names)
   end
 end
 
-function binarySearch(table, element)
-    local inf = 1
-    local sup = #table
-    while inf <= sup do
-        local meio = math.floor((inf + sup) / 2)
-        if element == table[meio] then
-            return true
-        elseif element < table[meio] then
-            sup = meio - 1
-        else
-            inf = meio + 1
-        end
-    end
-    return false
-end
-
 function main()
   local names = {}
   local n = #sampleNames
   for i=1,testN do
-    names[i] = sampleNames[((i-1) % n)+1]  .. i
+    names[i] = sampleNames[((i-1) % n)+1] .. i
   end
-  
+
   registerUser('Fulano')
   registerUsers(names)
   registerUser('Beltrano')
@@ -47,4 +45,3 @@ local t0 = os.clock()
 main()
 local t1 = os.clock()
 print(string.format("Tempo de execucao: %.4f segundos", t1 - t0))
-
